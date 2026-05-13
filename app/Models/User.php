8cc00b31662539d4265c05aa947c299a2f33359a
SALVADOR <coutsalv4@gmail.com>
2026-05-13 04:26:48 -0600

    feat(users): actualizar modelo User
    -Agregar must_change_password en Fillable
    -Agregar cast boolean para must_change_password

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'lastname', 'email', 'phone', 'role', 'password'])]
+#[Fillable(['name', 'lastname', 'email', 'phone', 'role', 'password', 'must_change_password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -28,6 +28,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'must_change_password' => 'boolean',
         ];
     }
 }
