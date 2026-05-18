43d98a7020d010bf210ca7a6b6dff4048ce14bfd
SALVADOR <coutsalv4@gmail.com>
2026-05-18 17:30:16 -0600

    refactor: mover register dentro del middleware

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\AuthController;
 
 Route::post('login', [AuthController::class, 'login']);
-Route::post('register', [AuthController::class, 'register']);
-Route::get('services', [ServiceController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::post('register', [AuthController::class, 'register']);
+    
     Route::post('change-password', [AuthController::class, 'changePassword']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
