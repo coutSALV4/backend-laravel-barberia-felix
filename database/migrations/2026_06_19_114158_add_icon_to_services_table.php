e7829aa1ac26ef66962ffa37d2740d45575c0cbc
SALVADOR <coutsalv4@gmail.com>
2026-06-19 11:44:31 -0600

    fix:arreglar migracion de icon en services

--- database/migrations/2026_06_19_114158_add_icon_to_services_table.php
+++ database/migrations/2026_06_19_114158_add_icon_to_services_table.php
@@ -21,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('services');
+        Schema::table('services', function (Blueprint $table) {
+            $table->dropColumn('icon');
+        });
     }
 };
